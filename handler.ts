31312f80f9f11b02cac7a85a5df57855ccd7746b
Ucchishta Sivaguru <ucc.men@gmail.com>
2021-03-27 18:06:59 +0800

    implement getTemplateAdf function that uses adf-utils

--- handler.ts
+++ handler.ts
@@ -1,4 +1,5 @@
 import {APIGatewayEvent} from 'aws-lambda';
+import {ADFEntity, filter} from '@atlaskit/adf-utils';
 
 export const hello = async (event: APIGatewayEvent) => {
   return {
@@ -6,6 +7,23 @@ export const hello = async (event: APIGatewayEvent) => {
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
       input: event,
+    }, null, 2),
+  };
+}
+
+export const getTemplateAdf = async (event: APIGatewayEvent) => {
+  const fieldGroupMacro = filter(
+    {type: 'something'},
+    (node: ADFEntity) =>
+      node.attrs?.extensionKey === 'field-group' &&
+      node.attrs?.parameters?.macroMetadata.macroId?.value === 'someId'
+  );
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+      fieldGroupMacro,
     }, null, 2),
   };
 }
